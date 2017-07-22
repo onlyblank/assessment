141c67d1b15306596c226a29c56f46a9aed63ff0
Blanca Perez <blankp@gmail.com>
2017-07-22 14:13:25 -0400

    Added tests for units

--- js-numerals/test/test-main.js
+++ js-numerals/test/test-main.js
@@ -4,5 +4,13 @@ var NumberToWords = require('../src/main.js');
 describe('numberToWords', function() {
     it('should return units in english', function() {
         assert.equal(NumberToWords.numberToWords(1), "one");
+        assert.equal(NumberToWords.numberToWords(2), "two");
+        assert.equal(NumberToWords.numberToWords(3), "three");
+        assert.equal(NumberToWords.numberToWords(4), "four");
+        assert.equal(NumberToWords.numberToWords(5), "five");
+        assert.equal(NumberToWords.numberToWords(6), "six");
+        assert.equal(NumberToWords.numberToWords(7), "seven");
+        assert.equal(NumberToWords.numberToWords(8), "eight");
+        assert.equal(NumberToWords.numberToWords(9), "nine");
     });
 });

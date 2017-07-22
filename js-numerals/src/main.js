19041c8bf0f61b7fe69aed9164355adc03b21d3b
Blanca Perez <blankp@gmail.com>
2017-07-22 18:11:52 -0400

    Numbers 1 - 99 tests in green

--- js-numerals/src/main.js
+++ js-numerals/src/main.js
@@ -1,16 +1,27 @@
 function numberToWords(number) {
-	return (Math.floor(number / 10) < 1) ? units(number-1) : teens(number%10)	
+	return (Math.floor(number / 10) < 1) ? units(number) : teens(number)	
 }
 
 function units(index){
 	var units = ["one", "two", "three", "four", "five", "six" , "seven", "eight", "nine"]
-	return units[index]
+
+	return units[index-1]
 }
 
-function teens(index){
-	var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-	return teens[index]
+function teens(number){
+	var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+	var tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+	
+	var quo = Math.floor(number / 10);
+	var rem = number % 10;
+
+	if(quo > 1){
+		return `${tens[quo-2]} ${rem > 0 ? units(rem) : ""}`.trim()
+	}else{
+		return teens[rem];
+	}
 }
+
 
 
 if (typeof exports !== 'undefined') {

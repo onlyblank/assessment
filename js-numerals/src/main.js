7be028ffd2357a570a6a46b03a01c7e097b6c249
Blanca Perez <blankp@gmail.com>
2017-07-22 22:55:49 -0400

    Refactoring - Putting all arrays with tranlastions availables

--- js-numerals/src/main.js
+++ js-numerals/src/main.js
@@ -1,28 +1,31 @@
-function numberToWords(number) {
-	return (Math.floor(number / 10) < 1) ? units(number) : teens(number)	
-}
+function NumberToWords(){
+	this.units = ["one", "two", "three", "four", "five", "six" , "seven", "eight", "nine"];
+	this.teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+	this.tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
-function units(index){
-	var units = ["one", "two", "three", "four", "five", "six" , "seven", "eight", "nine"]
+	this.getUnits = function(index){
+		return this.units[index-1]
+	};
 
-	return units[index-1]
-}
+	this.getTeens = function(number){
+		var quo = Math.floor(number / 10);
+		var rem = number % 10;
 
-function teens(number){
-	var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-	var tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-	
-	var quo = Math.floor(number / 10);
-	var rem = number % 10;
-
-	if(quo > 1){
-		return `${tens[quo-2]} ${rem > 0 ? units(rem) : ""}`.trim()
-	}else{
-		return teens[rem];
+		if(quo > 1){
+			return `${this.tens[quo-2]} ${rem > 0 ? this.getUnits(rem) : ""}`.trim()
+		}else{
+			return this.teens[rem];
+		}
+	};
+	this.translator = function(number) {
+		return (Math.floor(number / 10) < 1) ? this.getUnits(number) : this.getTeens(number)
 	}
 }
 
-
+function numberToWords(number){
+	var ntw = new NumberToWords();
+	return ntw.translator(number);	
+}
 
 if (typeof exports !== 'undefined') {
   exports.numberToWords = numberToWords;

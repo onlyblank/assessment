48eb1aafcdc09bf6eacf393ef212d99435d598e6
Blanca Perez <blankp@gmail.com>
2017-07-24 00:02:49 -0400

    Added new tasks for styles and html

--- js-numerals/gulpfile.js
+++ js-numerals/gulpfile.js
@@ -1,7 +1,42 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const sass = require('gulp-sass');
+const inject = require('gulp-inject');
 
 gulp.task('tests', function() {
   return gulp.src(['test/test-*.js'], {read: false})
 	.pipe(mocha({reporter: 'nyan'}))
+});
+ 
+gulp.task('styles', function(){
+  var injectAppFiles = gulp.src('src/assets/sass/*.scss', {read: false});
+ 
+  function transformFilepath(filepath) {
+    return '@import "' + filepath + '";';
+  }
+ 
+  var injectAppOptions = {
+    transform: transformFilepath,
+    starttag: '// inject:app',
+    endtag: '// endinject',
+    addRootSlash: false
+  };
+ 
+  return gulp.src('src/assets/sass/main.scss')
+    .pipe(inject(injectAppFiles, injectAppOptions))
+    .pipe(sass())
+    .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('html', ['styles'],function(){
+	var injectFiles = gulp.src(['dist/styles/main.css']);
+
+	var injectOptions = {
+    addRootSlash: false,
+    ignorePath: ['src', 'dist']
+  };
+
+  return gulp.src('src/index.html')
+  	.pipe(inject(injectFiles, injectOptions))
+    .pipe(gulp.dest('dist'))
 });

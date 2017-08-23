59f62caba3ff31a8beb2627c909810d43edde7be
Blanca Perez <blankp@gmail.com>
2017-08-22 23:39:02 -0300

    Fixed overflow on the screen when the number is too big

--- js-numerals/src/assets/js/main.js
+++ js-numerals/src/assets/js/main.js
@@ -20,17 +20,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		var number = document.getElementById("number");
 		var words = document.getElementById("words");
 		if(number.value != ""){
+			var newH2 = document.createElement("h2");
+			var numberText = numberToWords(number.value).toString();
+			var newNumber = document.createTextNode(numberText);
 			words.removeChild(words.lastChild);
-			var newH2 = document.createElement("h2"); 
-			var newNumber = document.createTextNode(numberToWords(number.value).toString()); 
 			newH2.appendChild(newNumber);
 			newH2.setAttribute("class", "reveal-text" );
 			words.appendChild(newH2);
+			number.value = "";
 			toggleShow();
-			number.value = "";			
+			fitText(numberText, newH2);		
 		}
 	};
 
+	var fitText = function(text, el){
+		var sizeStart = 12;
+		while(el.offsetHeight < el.scrollHeight || el.offsetWidth < el.scrollWidth){
+			sizeStart -= 0.2;
+			el.style.fontSize = `${sizeStart}vw`; 
+		}
+	}
 	var catchEscBackspace = function(e){
 		var event = e || window.event;
 		var key = event.keyCode || event.which;
@@ -39,20 +48,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	var toggleShow = function(){
 		var words = document.getElementById("words");
-		console.log("entro");
 		if(words.classList.contains('show')){
-			console.log('latiene');
 			document.querySelector("body").removeEventListener("keydown", catchEscBackspace);
 		}else{
-			console.log('nolatiene')
 			document.querySelector("body").addEventListener("keydown", catchEscBackspace);
 		}
 		words.classList.toggle('show');
 	};
 
-	document.getElementById("get-text").addEventListener("click", translateInput);
-
 	document.querySelector("#words .undo").addEventListener("click", toggleShow);
-
 	document.getElementById("number").addEventListener("keypress", validateNumbers);
+	document.getElementById("get-text").addEventListener("click", translateInput);
 })
